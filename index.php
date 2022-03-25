38fd01c8ea95dbb9ea7724837ba709bc2c28480f
nurnaby <sohag.biit@gmail.com>
2022-03-25 11:58:38 +0600

    update assignment class 2

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+ /*Case Study:
+
+ I have three old hp monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk.
+
+For creating a product upload form what specifications will be uploaded and what will be their data types?
+*/
+
+$product_name="hp";// datatype is strint 
+$product_status="old";
+$quantity=3;//datatype is integer
+$monitor_display=15.6;//datatype is double/float 
+$vga_cable_status= true; //datatype is boolean 
+$product_price=8000; //datatype is integer
+
+
+
+
+?>
